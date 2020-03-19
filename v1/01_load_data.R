23c12741ef5e78d5a55be83694c2a96f2c5bd287
mattsmitchell <mattsmitchell@gmail.com>
2020-03-18 22:17:41 -0700

    Load and tidy data

--- v1/01_load_data.R
+++ v1/01_load_data.R
@@ -1,11 +1,40 @@
 ## Load data
 library(readr)
+library(dplyr)
+library(tidyr)
 
 # Daily reports from John Hopkins University
-url_confirmed <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
-url_recovered <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
-url_deceased  <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
+confirmed_url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+recovered_url <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Recovered.csv"
+deceased_url  <- "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Deaths.csv"
 
-confirmed <- read_csv(url(url_confirmed))
-recovered <- read_csv(url(url_recovered))
-deceased  <- read_csv(url(url_deceased))
+# Population estimates 2019 (US Census Bureau)
+states_url <- "https://raw.githubusercontent.com/matt5mitchell/covid19/master/v1/states.csv"
+
+# Read data
+confirmed_raw <- read_csv(url(confirmed_url))
+recovered_raw <- read_csv(url(recovered_url))
+deceased_raw  <- read_csv(url(deceased_url))
+states <- read_csv(url(states_url))
+
+# Prepare data
+confirmed <- confirmed_raw %>% 
+  filter(`Country/Region` == "US",
+         `Province/State` %in% states$State) %>%
+  rename(State = `Province/State`) %>%
+  select(-`Country/Region`, -Lat, -Long) %>%
+  gather("Date", "Confirmed", -State)
+
+recovered <- recovered_raw %>% 
+  filter(`Country/Region` == "US",
+         `Province/State` %in% states$State) %>%
+  rename(State = `Province/State`) %>%
+  select(-`Country/Region`, -Lat, -Long) %>%
+  gather("Date", "Recovered", -State)
+
+deceased <- deceased_raw %>% 
+  filter(`Country/Region` == "US",
+         `Province/State` %in% states$State) %>%
+  rename(State = `Province/State`) %>%
+  select(-`Country/Region`, -Lat, -Long) %>%
+  gather("Date", "Confirmed", -State)
