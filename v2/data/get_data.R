029fca904740eb8d959a594bb6a7f060ebf1e3cc
mattsmitchell <mattsmitchell@gmail.com>
2020-03-25 00:22:51 -0700

    Added population data for counties

--- v2/data/get_data.R
+++ v2/data/get_data.R
@@ -5,13 +5,18 @@ library(readr)
 library(dplyr)
 library(tidyr)
 library(lubridate)
+library(purrr)
 
 ## Population data ----
 
-# Population estimates 2019 (US Census Bureau)
-states_url <- "https://raw.githubusercontent.com/matt5mitchell/covid19/master/v1/states.csv"
-states <- read_csv(url(states_url))
+# Population estimates (US Census Bureau) - 2019 for states, 2018  for counties/cities
+states_url <- "https://raw.githubusercontent.com/matt5mitchell/covid19/master/v2/data/states.csv"
+counties_url <- "https://raw.githubusercontent.com/matt5mitchell/covid19/master/v2/data/counties.csv"
+cities_url <- "https://raw.githubusercontent.com/matt5mitchell/covid19/master/v2/data/cities.csv"
 
+states <- read_csv(url(states_url))
+counties <- read_csv(url(counties_url))
+cities <- read_csv(url(cities_url))
 
 ## Johns Hopkins data formatting changed
 
@@ -60,9 +65,10 @@ data_cases_1 <- confirmed %>%
   filter(Date >= ymd(20200310), #First cases in data
          Date < ymd(20200322)) %>% #New format beginning 2020-3-22
   mutate(FIPS = NA,
-         Place = NA) %>%
-  select(Date, FIPS, Place, State,
-         Confirmed, Recovered, Deaths)
+         County = NA) %>%
+  dplyr::select(Date, FIPS, State, County,
+         Confirmed, Recovered, Deaths) %>%
+  inner_join(states, by="State") #Add population & filter to 50 states + DC
 
 ## Dataset 2 3/23/2020 onward ----
 date_start <- ymd(20200322)
@@ -92,15 +98,22 @@ data_daily_reports <- date_vector %>% map(get_daily_report) %>% bind_rows()
 
 # Prepare data
 data_cases_2 <- data_daily_reports %>%
-  rename(Place = Admin2,
+  rename(County = Admin2,
          State = Province_State) %>%
-  select(Date, FIPS, Place, State,
-         Confirmed, Recovered, Deaths)
+  mutate(FIPS = ifelse(nchar(FIPS) == 4, paste0("0", FIPS), FIPS)) %>% #Add leading zero
+  left_join(counties, by="FIPS") %>% #add county population
+  inner_join(states, by="State") %>% #filter rows and add state population
+  mutate(FIPS = ifelse(is.na(FIPS.x), FIPS.y, FIPS.x), #if county is blank...
+         Population = ifelse(is.na(FIPS.x), Population.y, Population.x)) %>% 
+  rename(County = County.y) %>% #use census bureau names
+  dplyr::select(Date, FIPS, State, County,
+         Confirmed, Recovered, Deaths, Population)
 
 ## Final Dataset of Cases ----
 
 # Row bind datasets
-data_cases <- data_cases_1 %>% bind_rows(data_cases_2)
+data_cases <- data_cases_1 %>% 
+  bind_rows(data_cases_2)
 
 # Save data
 write_csv(data_cases, path = "data_cases.csv")
